378733f726511f3e0e39a5929dc5452526d57d73
Alexey <alekseiovechkin@gmail.com>
2019-07-06 10:38:14 +0300

    5 1 HW4

--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,10 +1,14 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.Util;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -13,6 +17,7 @@ import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryMealRepository implements MealRepository {
+    private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
 
     // Map  userId -> (mealId-> meal)
     private Map<Integer, InMemoryBaseRepository<Meal>> usersMealsMap = new ConcurrentHashMap<>();
@@ -22,6 +27,16 @@ public class InMemoryMealRepository implements MealRepository {
         Objects.requireNonNull(meal, "meal must not be null");
         var meals = usersMealsMap.computeIfAbsent(userId, uid -> new InMemoryBaseRepository<>());
         return meals.save(meal);
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        log.info("+++ PostConstruct");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        log.info("+++ PreDestroy");
     }
 
     @Override
